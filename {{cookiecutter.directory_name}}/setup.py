2ff0724e18a57417c7a5a3fc5bacb2dee47ae75c
Rotem Yaari <vmalloc@gmail.com>
2018-07-17 22:37:23 +0300

    Initial commit

---
+++ {{cookiecutter.directory_name}}/setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    setup_requires=['pbr>=3.0', 'setuptools>=17.1'],
+    pbr=True,
+    long_description_content_type='text/markdown; charset=UTF-8',
+)
